58716cda09b25b8e85d9c6366fff5f502844ed69
k0825 <kazukkiyo@icloud.com>
2022-12-13 23:20:05 +0900

    トレイト

---
+++ 10/traits/src/main.rs
@@ -0,0 +1,29 @@
+use traits::NewArticle;
+use traits::Summary;
+use traits::Tweet;
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        // アメリカ、ペンシルベニア州、ピッツバーグ
+        location: String::from("Pittsburgh, PA, USA"),
+        // アイスバーグ
+        author: String::from("Iceburgh"),
+        // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+}
