6ba01b519505eb190bc005f08e12e6635294c894
k0825 <kazukkiyo@icloud.com>
2023-03-23 00:48:06 +0900

    14章

--- 10/lifetime/src/main.rs
+++ 10/lifetime/src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
-    let s1 = String::from("long string is long");
-    let result;
-    {
-        let s2 = String::from("abc");
-        result = longest(s1.as_str(), s2.as_str());
-    }
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    // 最長の文字列は、{}です
     println!("The longest string is {}", result);
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest(x: &str, y: &str) -> &str {
     if x.len() > y.len() {
         x
     } else {
